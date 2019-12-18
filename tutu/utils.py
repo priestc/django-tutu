1dc9bf88a302d1a3de002880229715dca5b671ca
Chris Priest <cp368202@ohiou.edu>
2019-12-18 15:56:45 -0700

    code from video 8

--- tutu/utils.py
+++ tutu/utils.py
@@ -1,4 +1,5 @@
 from graphsets import Graphset
+import datetime
 
 def validate_graphset(graphset):
     if isinstance(graphset, Graphset):
@@ -8,8 +9,25 @@ def validate_graphset(graphset):
     else:
         raise ValueError("Must be a Graphset class or instance")
 
-def make_test_ticks():
-    pass
+def make_test_ticks(start_ago=datetime.timedelta(days=30)):
+    from tutu.models import Tick
+    now = datetime.datetime.now()
+    target = now - start_ago
+    while(target < now):
+        Tick.objects.create(date=target, machine="TestMachine")
+        target += datetime.timedelta(minutes=5)
 
-def make_poll_results():
-    pass
+def make_poll_results(graphsets):
+    import random
+    from tutu.models import Tick, PollResult
+    for tick in Tick.objects.all():
+        for item in graphsets:
+            graphset = validate_graphset(item)
+            result = random.random()
+            PollResult.objects.create(
+                tick=tick,
+                graphset_name=graphset.get_name(),
+                result=result,
+                success=True,
+                seconds_to_poll=1
+            )
---
+++ tutu/views.py
@@ -0,0 +1,8 @@
+def list_machines(request):
+    pass
+
+def show_machine_graphs(request, machine):
+    pass
+
+def show_graph(request, machine, graphset, interval):
+    pass
